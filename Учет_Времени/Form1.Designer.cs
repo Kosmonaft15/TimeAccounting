1f35b36d0a2cabe769224933ddac854de15c846f
Хрунов Александр Евгеньевич <khrunoff@gmail.com>
2018-08-13 12:42:14 +0300

    Initial

---
+++ Учет_Времени/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Учет_Времени
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(12, 39);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(500, 449);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Имя программы";
+            this.columnHeader1.Width = 162;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Время работы";
+            this.columnHeader2.Width = 109;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Время запуска";
+            this.columnHeader3.Width = 108;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Время работы";
+            this.columnHeader4.Width = 97;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество запусков: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(437, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 500);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Время";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Учет_Времени/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Xml;
+
+namespace Учет_Времени
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //счетчик запусков программы
+            Properties.Settings.Default.total_runs++;
+            Properties.Settings.Default.Save(); //сохранение переменных приложения
+            label1.Text += Properties.Settings.Default.total_runs.ToString(); //вывод кол-ва запусков программы
+
+            Process[] procs = Process.GetProcesses();
+            foreach (Process p in procs) //получить список процессов и время
+            {
+                try
+                {
+                    ListViewItem row = new ListViewItem(p.ProcessName);
+                    row.SubItems.Add(p.TotalProcessorTime.TotalMinutes.ToString());
+                    row.SubItems.Add(p.StartTime.ToString(@"hh\:mm\:ss"));
+                    row.SubItems.Add((DateTime.Now - p.StartTime).ToString(@"hh\:mm\:ss"));
+                    listView1.Items.Add(row);                    
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+
+            save_data(); ////сохранение в XML
+        }
+
+        private void button1_Click(object sender, EventArgs e) //кнопка Обновить
+        {
+            listView1.BeginUpdate();
+            listView1.Items.Clear();
+
+            try //пересоздание listView1
+            {
+                Process[] procs = Process.GetProcesses();
+                foreach (Process p in procs)
+                {
+                    try
+                    {
+                        ListViewItem row = new ListViewItem(p.ProcessName);
+                        row.SubItems.Add(p.TotalProcessorTime.TotalMinutes.ToString());
+                        row.SubItems.Add(p.StartTime.ToString(@"hh\:mm\:ss"));
+                        row.SubItems.Add((DateTime.Now - p.StartTime).ToString(@"hh\:mm\:ss"));
+                        listView1.Items.Add(row);
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            finally
+            {
+                listView1.EndUpdate();
+            }
+        }
+
+        private void save_data()//сохранение в XML
+        {          
+            XmlDocument xmlData = new XmlDocument();
+            XmlNode rootNode = xmlData.CreateElement("process");
+            xmlData.AppendChild(rootNode);
+
+            XmlNode procesNode = xmlData.CreateElement("proces");
+            XmlAttribute attributeProcName = xmlData.CreateAttribute("name");
+            XmlAttribute attributeProcTotalProcessorTime = xmlData.CreateAttribute("total_time");
+            XmlAttribute attributeProcStartTime = xmlData.CreateAttribute("start_time");
+            XmlAttribute attributeProcWorkTime = xmlData.CreateAttribute("work_time");
+
+            attributeProcName.Value = listView1.Items[0].SubItems[0].Text;
+            attributeProcTotalProcessorTime.Value = listView1.Items[0].SubItems[1].Text;
+            attributeProcStartTime.Value = listView1.Items[0].SubItems[2].Text;
+            attributeProcWorkTime.Value = listView1.Items[0].SubItems[3].Text;
+
+            procesNode.Attributes.Append(attributeProcName);
+            procesNode.Attributes.Append(attributeProcTotalProcessorTime);
+            procesNode.Attributes.Append(attributeProcStartTime);
+            procesNode.Attributes.Append(attributeProcWorkTime);
+
+            rootNode.AppendChild(procesNode);
+            xmlData.Save("process_data.xml");
+        }
+    }
+}
